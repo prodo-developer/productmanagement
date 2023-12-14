216debdfb48e3d12a48b5212b2f94042ccdba3b8
prodo-developer <wjdals300@gmail.com>
2023-12-14 20:26:37 +0900

    Initial commit

---
+++ spring-study/petnote/src/main/java/space/api/petnote/core/feature/common/Period.java
@@ -0,0 +1,23 @@
+package space.api.petnote.core.feature.common;
+
+import java.time.LocalDateTime;
+
+public class Period {
+
+    public LocalDateTime startDate;
+    public LocalDateTime endDate;
+
+    public Period(LocalDateTime startDate, LocalDateTime endDate) {
+        this.startDate = startDate.withHour(0).withMinute(0).withSecond(0);
+        this.endDate = endDate.withHour(23).withMinute(59).withSecond(59);
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+}
---
+++ spring-study/spring-ldapDemo/src/main/java/com/example/springldapdemo/UserInfo.java
@@ -0,0 +1,2 @@
+package com.example.springldapdemo;public class UserInfo {
+}
